5b22f680106853f430d239bb58fe32484507f77a
Alexey B <bikchurin2001@mail.ru>
2022-07-05 19:23:41 +0300

    Initial commit

---
+++ src/main/kotlin/com/miumiuhaskeer/fastmessage/statistic/FastMessageStatisticApplication.kt
@@ -0,0 +1,11 @@
+package com.miumiuhaskeer.fastmessage.statistic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastMessageStatisticApplication
+
+fun main(args: Array<String>) {
+    runApplication<FastMessageStatisticApplication>(*args)
+}
---
+++ src/test/kotlin/com/miumiuhaskeer/fastmessage/statistic/FastMessageStatisticApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.miumiuhaskeer.fastmessage.statistic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastMessageStatisticApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
