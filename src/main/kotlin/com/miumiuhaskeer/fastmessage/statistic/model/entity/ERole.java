c26523ce02d052914aad59a69c6d7c2dea3314ed
Alexey B <bikchurin2001@mail.ru>
2022-07-16 22:00:29 +0300

    Migrate ERole to Kotlin

--- src/main/kotlin/com/miumiuhaskeer/fastmessage/statistic/model/entity/ERole.java
+++
@@ -1,6 +0,0 @@
-package com.miumiuhaskeer.fastmessage.statistic.model.entity;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_ADMIN
-}
---
+++ src/main/kotlin/com/miumiuhaskeer/fastmessage/statistic/model/entity/ERole.kt
@@ -0,0 +1,6 @@
+package com.miumiuhaskeer.fastmessage.statistic.model.entity
+
+enum class ERole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
